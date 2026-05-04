3b63a369a2ddf0fb5480e8bcecc6bd0965093d9c
shanto <shantohasibulhasan379@gmail.com>
2026-05-04 18:04:01 +0600

    added country

---
+++ react-world-on-the-go/src/components/Country/Country.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Country = ({ country, handleCountVisit }) => {
+    // console.log(country)
+    const [visited, setVisited] = useState(false)
+    const handleVisited = ()=>{
+        console.log('visited')
+        setVisited(!visited)
+        handleCountVisit()
+    }
+
+    
+    return (
+        <div>
+            <div className={`parent ${visited ? 'visited' : 'Not Visited'}`}>
+                <img className="img" src={country.flags.flags.png} alt={country.flags.flags.alt} /><br />
+                <strong>
+                    Official Name: {country.name.common}
+                </strong>
+                <p>
+                    Common Name: {country.name.official}
+                </p>
+                <p>
+                    Capital Name: {country.capital.capital}
+                </p>
+                <p>
+                    Region Name: {country.region.region}
+                </p>
+                <p>
+                    Language Name: {country.languages.languages.eng}
+                </p>
+                <button onClick={handleVisited}>
+                    {visited ? 'Visited' : 'Not Visited'}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Country;
--- react-world-on-the-go/src/components/countries/Countries.jsx
+++ react-world-on-the-go/src/components/countries/Countries.jsx
@@ -1,12 +1,28 @@
-import { use } from "react";
+import { use, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = ({ fetchCountries }) => {
     const countriesData = use(fetchCountries)
     const countries = countriesData.countries
+    const [visitedCountries, setVisitedCountries] = useState(0)
+    const handleCountVisit = () => {
+        console.log('visited country')
+        const newVisit = visitedCountries + 1
+        setVisitedCountries(newVisit)
+    }
     return (
-        <div>
+        <>
             <h1>All countries are {countries.length}</h1>
-        </div>
+            <strong>Visited countries <span onClick={setVisitedCountries}>0</span></strong>
+            <div className="countries">
+                {
+                    countries.map(country =><Country 
+                        country={country}
+                        handleCountVisit={handleCountVisit}
+                        ></Country>)
+                }
+            </div>
+        </>
     );
 };
 
